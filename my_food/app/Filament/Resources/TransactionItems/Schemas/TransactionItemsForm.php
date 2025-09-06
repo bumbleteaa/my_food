63ad2f530f4777cb81f96297539cb2bdd112a72a
bumbleteaa <lukitrisetiawan2505@gmail.com>
2025-09-06 21:25:22 +0700

    Fix: add transaction item

---
+++ my_food/app/Filament/Resources/TransactionItems/Schemas/TransactionItemsForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\TransactionItems\Schemas;
+
+use Filament\Schemas\Schema;
+
+class TransactionItemsForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}

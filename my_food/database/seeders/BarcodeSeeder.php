528ce1c451f5007ec25b17d1771984ea48dc590d
bumbleteaa <lukitrisetiawan2505@gmail.com>
2025-10-11 22:46:15 +0700

    adding bacon qr

--- my_food/database/seeders/BarcodeSeeder.php
+++ my_food/database/seeders/BarcodeSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Barcode;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class BarcodeSeeder extends Seeder
